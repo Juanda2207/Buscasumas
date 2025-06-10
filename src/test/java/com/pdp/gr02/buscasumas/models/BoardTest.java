2bbdd2fa89508bddef70e1bf119537b71fe123e9
Alejandro Baez Muñoz <alejandrobaezmunoz@gmail.com>
2025-06-10 18:52:31 -0500

    Board & Cells tests

--- src/test/java/com/pdp/gr02/buscasumas/models/BoardTest.java
+++ src/test/java/com/pdp/gr02/buscasumas/models/BoardTest.java
@@ -6,5 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
     @Test
+    void testBoardInitialization() {
+        Board board = new Board(5, 5, 5);
 
+        assertNotNull(board, "Board should be initialized");
+        assertEquals(5, board.GetRows(), "Board should have 5 rows");
+        assertEquals(5, board.GetColumns(), "Board should have 5 columns");
+        assertEquals(5, board.GetTotalMines(), "Board should have 5 mines");
+
+    }
 }
--- src/test/java/com/pdp/gr02/buscasumas/views/MainAppViewTest.java
+++ src/test/java/com/pdp/gr02/buscasumas/views/MainAppViewTest.java
@@ -5,6 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainAppViewTest {
-    @Test
+
 
 }
